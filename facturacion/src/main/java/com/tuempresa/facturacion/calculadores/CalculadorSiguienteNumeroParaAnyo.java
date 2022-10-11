4be864ef5813f470ad93ad9f4329483ad0899b38
Carolina Laguna <114037038+carolinaLagunaSV@users.noreply.github.com>
2022-10-10 21:23:54 -0500

    Leccion 7

--- facturacion/src/main/java/com/tuempresa/facturacion/calculadores/CalculadorSiguienteNumeroParaAnyo.java
+++ facturacion/src/main/java/com/tuempresa/facturacion/calculadores/CalculadorSiguienteNumeroParaAnyo.java
@@ -13,7 +13,7 @@ public class CalculadorSiguienteNumeroParaAnyo
 		int anyo;
 		 public Object calculate() throws Exception { 
 		Query query = XPersistence.getManager() 
-	    .createQuery("select max(f.numero) from Factura f where f.anyo = :anyo");
+	    .createQuery("select max(f.numero) from DocumentoComercial f where f.anyo = :anyo");
 		query.setParameter("anyo",anyo);
 		Integer ultimoNumero = (Integer) query.getSingleResult();
 		return ultimoNumero == null ? 1 : ultimoNumero + 1;
--- facturacion/src/main/java/com/tuempresa/facturacion/modelo/Detalle.java
+++ facturacion/src/main/java/com/tuempresa/facturacion/modelo/Detalle.java
@@ -8,6 +8,6 @@ public class Detalle {
 	int cantidad;
 	 @ManyToOne(fetch = FetchType.LAZY, optional = true)
 	 Producto producto;
-
+ 
 }
 
